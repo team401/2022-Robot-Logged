2473fa4c81de7544f0ea42ec922c8b2f588413ab
Cameron Earle <camearle20@me.com>
2022-03-14 15:47:13 -0400

    Add comments to Vision + led control methods

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -12,20 +12,42 @@ import org.littletonrobotics.junction.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Subsystem implementing vision.  This subsystem takes in data sent over NetworkTables by the limelight pointed at the
+ * target, sorts the detected corners of each piece of vision tape, finds the vector pointing from the camera to each
+ * corner, and fits a circle with the known radius of the goal circle to the detected corners, ultimately calculating
+ * the vector from the camera lens to the center of the goal.  This vector is then reported to RobotState using the
+ * "recordVisionObservations" method.
+ *
+ * This subsystem also provides methods for turning the LEDs on and off.
+ *
+ * REQUIRE THIS SUBSYSTEM IN COMMANDS IF: you need to control the limelight LEDs.  Vision data is always recorded to
+ *                                        RobotState automatically, so commands don't need to worry about that.
+ */
 public class Vision extends SubsystemBase {
     private final VisionIO.VisionIOInputs inputs = new VisionIO.VisionIOInputs();
     private final VisionIO io;
 
+    // Timestamp in seconds that the last image was captured at
     private double lastCaptureTimestamp = 0.0;
-    private boolean ledsOn = true;
 
+    // Whether or not the LEDs should be on.
+    private boolean ledsOn = false;
+
+    // Field of view constants for the limelight at 1x zoom
     private static final double vpw =
             2.0 * Math.tan(VisionConstants.fovHorizontal.getRadians() / 2.0);
     private static final double vph =
             2.0 * Math.tan(VisionConstants.fovVertical.getRadians() / 2.0);
 
-    private static final int minTargetCount = 2; // Minimum number of tape targets to fit a circle
+    // Minimum number of pieces of reflective tape that must be seen to constitute a target detection.
+    private static final int minTargetCount = 2;
+
+    // Precision that must be achieved by the circle fitter, in meters.
     private static final double circleFitPrecision = 0.01;
+
+    // Constant latency applied to the timestamps we get from NetworkTables, accounting for latency in image capture
+    // and transport over the network that cannot be measured in software.
     private static final double constantLatency = 0.06;
 
     public Vision(VisionIO io) {
@@ -37,8 +59,10 @@ public class Vision extends SubsystemBase {
         io.updateInputs(inputs);
         Logger.getInstance().processInputs("Vision", inputs);
 
+        io.setLeds(ledsOn);
+
         // If there is no new frame, do nothing
-        //if (inputs.captureTimestamp == lastCaptureTimestamp) return;
+        if (inputs.captureTimestamp == lastCaptureTimestamp) return;
         lastCaptureTimestamp = inputs.captureTimestamp;
 
         // Grab target count from corners array.  If LEDs are off, force
@@ -87,6 +111,12 @@ public class Vision extends SubsystemBase {
         RobotState.getInstance().recordVisionObservations(lastCaptureTimestamp - constantLatency, cameraToTargetTranslation);
     }
 
+    /**
+     * Sorts a list of corners found in the image to the expected order for vision processing.
+     * @param corners The list of all corners
+     * @param average The point that is the average of all corners (geometric center)
+     * @return Sorted list of corners
+     */
     private List<VisionPoint> sortCorners(List<VisionPoint> corners,
                                           VisionPoint average) {
 
@@ -154,20 +184,36 @@ public class Vision extends SubsystemBase {
         return newCorners;
     }
 
+    /**
+     * Given a list of corners, solves for the translation "camera to target", meaning the vector that comes out of
+     * the camera's lens and points into the center of the target.
+     *
+     * This is done by using the known height of the camera and point of interest, and using the difference of them
+     * combined with the camera's angle, field of view, and image resolution to correct for the perspective of the camera.
+     *
+     * @param corner The point to process
+     * @param goalHeight The known height of the point in world space
+     * @param cameraHeight The known height of the camera in world space
+     * @param floorToCamera The rotation between the floor and the lens, in the floor plane frame of reference.
+     * @return The vector from the camera to the target.
+     */
     private Translation2d solveCameraToTargetTranslation(VisionPoint corner,
                                                          double goalHeight, double cameraHeight, Rotation2d floorToCamera) {
 
+        // Compute constants for going from pixel space to camera space
         double halfWidthPixels = VisionConstants.widthPixels / 2.0;
         double halfHeightPixels = VisionConstants.heightPixels / 2.0;
         double nY = -((corner.x - halfWidthPixels) / halfWidthPixels);
         double nZ = -((corner.y - halfHeightPixels) / halfHeightPixels);
 
+        // Rotate by camera angle
         Translation2d xzPlaneTranslation = new Translation2d(1.0, vph / 2.0 * nZ)
                 .rotateBy(floorToCamera);
         double x = xzPlaneTranslation.getX();
         double y = vpw / 2.0 * nY;
         double z = xzPlaneTranslation.getY();
 
+        // Perspective correction
         double differentialHeight = cameraHeight - goalHeight;
         if ((z < 0.0) == (differentialHeight > 0.0)) {
             double scaling = differentialHeight / -z;
@@ -179,6 +225,9 @@ public class Vision extends SubsystemBase {
         return null;
     }
 
+    /**
+     * Class representing pixel coordinates for a point of interest in the camera's image.
+     */
     public static class VisionPoint {
         public final double x;
         public final double y;
@@ -189,14 +238,17 @@ public class Vision extends SubsystemBase {
         }
     }
 
-    public static class TimestampedTranslation2d {
-        public final double timestamp;
-        public final Translation2d translation;
+    /**
+     * Turns on the limelight LEDs
+     */
+    public void turnOnLeds() {
+        ledsOn = true;
+    }
 
-        public TimestampedTranslation2d(double timestamp,
-                                        Translation2d translation) {
-            this.timestamp = timestamp;
-            this.translation = translation;
-        }
+    /**
+     * Turns off the limelight LEDs
+     */
+    public void turnOffLeds() {
+        ledsOn = false;
     }
 }
