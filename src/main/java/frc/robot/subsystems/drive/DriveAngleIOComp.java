c71c7c1c111405a1a77cb6bb1e9ee3845e7daa71
AethersHaven <sullivandovie@gmail.com>
2022-09-12 18:40:24 -0400

    Pigeon 2.0 Implementation

--- src/main/java/frc/robot/subsystems/drive/DriveAngleIOComp.java
+++ src/main/java/frc/robot/subsystems/drive/DriveAngleIOComp.java
@@ -1,21 +1,23 @@
 package frc.robot.subsystems.drive;
 
-import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.Pigeon2;
 
 import edu.wpi.first.math.util.Units;
 import frc.robot.Constants.CANDevices;
 
 public class DriveAngleIOComp implements DriveAngleIO {
 
-    private final PigeonIMU imu = new PigeonIMU(CANDevices.pigeonIMU);
+    private final Pigeon2 pigeon = new Pigeon2(CANDevices.pigeonIMU);
+
+    private double degOffset = 0;
 
     @Override
     public void updateInputs(DriveAngleIOInputs inputs) {
-        inputs.headingRad = Units.degreesToRadians(imu.getFusedHeading());
+        inputs.headingRad = Units.degreesToRadians(pigeon.getYaw() - degOffset);
     }
 
     @Override
     public void resetHeading() {
-        imu.setFusedHeading(0);        
+        degOffset = pigeon.getYaw();
     }  
 }
